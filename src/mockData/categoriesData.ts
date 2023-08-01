6002bb690554a527c639ea2e1394566965f487a4
KsanaSal <oasalivon@gmail.com>
2023-08-01 21:15:45 +0300

    + added mock data

---
+++ src/mockData/categoriesData.ts
@@ -0,0 +1,29 @@
+import iconListCheck from "../../images/icon-list-check.svg";
+import iconIdea from "../../images/icon-idea.svg";
+import iconRandomThought from "../../images/icon-random-thought.svg";
+import iconQuote from "../../images/icon-quote.svg";
+
+const categories = [
+    {
+        categoryId: "1",
+        categoryName: "Task",
+        categoryImg: iconListCheck,
+    },
+    {
+        categoryId: "2",
+        categoryName: "Random Thought",
+        categoryImg: iconRandomThought,
+    },
+    {
+        categoryId: "3",
+        categoryName: "Idea",
+        categoryImg: iconIdea,
+    },
+    {
+        categoryId: "4",
+        categoryName: "Quote",
+        categoryImg: iconQuote,
+    },
+];
+
+export default categories;

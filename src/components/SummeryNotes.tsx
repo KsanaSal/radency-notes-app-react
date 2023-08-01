14d9f79aa74bca9a5576df40dba2b5283de433f6
KsanaSal <oasalivon@gmail.com>
2023-08-01 15:03:14 +0300

    + added blanks files for tables

---
+++ src/components/SummeryNotes.tsx
@@ -0,0 +1,9 @@
+const SummeryNotes = () => {
+    return (
+        <div>
+            <h1>Summery Notes</h1>
+        </div>
+    );
+};
+
+export default SummeryNotes;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        {/* <Provider store={store}> */}
+        <App />
+        {/* </Provider> */}
+    </React.StrictMode>
+);
